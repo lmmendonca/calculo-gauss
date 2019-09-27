88121aafc7d5155e55cda6c8cb4a44cd8419852b
Elias Soares <elias_soares95@hotmail.com>
2019-09-26 23:48:14 -0300

    Alterações de exibição do resultado na interface

--- main.js
+++ main.js
@@ -1,28 +1,69 @@
 
-function calcularGauss(matriz, x, y) {
+function calcularGauss(matriz, x, y, etapas = []) {
   // Verifica se o pivo é zero
   if (matriz[x][y] === 0) {
-      console.log('Pivo zero', x, y);
+    console.log('Pivo zero', x, y);
 
-      for (let i = (x + 1); i < matriz.length; i++) {
-          if (matriz[i][y] === 0 && i === x) {
-              continue;
-          }
-
-          matriz = trocarLinha(matriz, x, i);
-          break;
+    for (let i = (x + 1); i < matriz.length; i++) {
+      if (matriz[i][y] === 0 && i === x) {
+        continue;
       }
+
+      matriz = trocarLinha(matriz, x, i);
+      break;
+    }
   }
 
   if (matriz[x][y] !== 0) {
-      console.log('Pivo', x, y);
-      matriz = calcularPivo(matriz, x, y);
+    console.log('Pivo', x, y);
+    matriz = calcularPivo(matriz, x, y);
   }
 
+  etapas.push(
+    JSON.parse(JSON.stringify(matriz))
+  );
+
   if (!isEscalonado(matriz)) {
-      matriz = calcularGauss(matriz, x + 1, y + 1);
-      return matriz;
+    matriz = calcularGauss(matriz, (x + 1), (y + 1), etapas);
+    return matriz;
   }
+
+  return {
+    matriz,
+    valorX: calcularX(JSON.parse(JSON.stringify(matriz))),
+    etapas,
+  };
+}
+
+
+function calcularPivo(matriz, x, y) {
+  // verificar se é a última linha
+  if (x !== matriz.length - 1) {
+    let valorA = matriz[x][y];
+
+    for (let i = x + 1; i < matriz.length; i++) {
+      let valorB = matriz[i][y];
+
+      if (valorB === 0) {
+        continue;
+      }
+
+      matriz[i] = somarArrays(
+        matriz[i],
+        multiplicarArray(matriz[x], ((-valorB) / valorA))
+      );
+    }
+
+    return matriz
+  }
+
+  let valorA = matriz[x - 1][y];
+  let valorB = matriz[x][y];
+
+  matriz[x] = somarArrays(
+    matriz[x],
+    multiplicarArray(matriz[x - 1], ((-valorB) / valorA))
+  );
 
   return matriz;
 }
@@ -40,7 +81,7 @@ function multiplicarArray(arrayX, numero) {
   let v = [];
 
   for (let i = 0; i < arrayX.length; i++) {
-      v[i] = arrayX[i] * numero;
+    v[i] = arrayX[i] * numero;
   }
 
   return v;
@@ -50,7 +91,7 @@ function somarArrays(array1, array2) {
   let novoArray = [];
 
   for (let i = 0; i < array1.length; i++) {
-      novoArray[i] = array1[i] + array2[i];
+    novoArray[i] = array1[i] + array2[i];
   }
 
   return novoArray;
@@ -60,46 +101,10 @@ function preencherLista(indice, max, valor) {
   let lista = [];
 
   for (; indice < max; indice++) {
-      lista.push(valor);
+    lista.push(valor);
   }
 
   return lista;
-}
-
-function calcularPivo(matriz, x, y) {
-  // verificar se é a última linha
-  if (x !== matriz.length - 1) {
-      let valorA = matriz[x][y];
-
-      for (let i = x + 1; i < matriz.length; i++) {
-          let valorB = matriz[i][y];
-
-          if (valorB === 0) {
-              continue;
-          }
-
-          matriz[i] = somarArrays(
-              matriz[i],
-              multiplicarArray(matriz[x], ((-valorB) / valorA))
-          );
-
-          console.log('matriz', matriz);
-      }
-
-      return matriz
-  }
-
-  let valorA = matriz[x - 1][y];
-  let valorB = matriz[x][y];
-
-  matriz[x] = somarArrays(
-      matriz[x],
-      multiplicarArray(matriz[x - 1], ((-valorB) / valorA))
-  );
-
-  console.log('matriz', matriz);
-
-  return matriz;
 }
 
 function isEscalonado(matrix) {
@@ -107,43 +112,41 @@ function isEscalonado(matrix) {
   let espacoAnterior = 0;
 
   for (let i = 0; (i < matrix.length); i++) {
-      for (let j = 0; j < comprimentoMaximo; j++) {
+    for (let j = 0; j < comprimentoMaximo; j++) {
 
-          if (matrix[i][j] === 0 || i === 0) {
-              continue;
-          }
-
-          if (j > espacoAnterior) {
-              espacoAnterior = j;
-          } else {
-              return false;
-          }
-
-          break;
+      if (matrix[i][j] === 0 || i === 0) {
+        continue;
       }
-  }
 
+      if (j > espacoAnterior) {
+        espacoAnterior = j;
+      } else {
+        return false;
+      }
+
+      break;
+    }
+  }
   return true;
 }
-
 
 function calcularX(matriz) {
   let resultado = [];
 
   for (let i = 0; i < matriz.length; i++) {
-      resultado.push(
-          matriz[matriz.length - 2]
-      );
+    resultado.push(
+      matriz[matriz.length - 2]
+    );
   }
 
-  comprimentoMatriz = matriz.length;
+  const comprimentoMatriz = matriz.length;
   resultado = preencherLista(0, comprimentoMatriz, 0);
 
   for (let i = (comprimentoMatriz - 1); (i > -1); i--) {
-      resultado[i] = matriz[i][comprimentoMatriz] / matriz[i][i];
-      for (let j = (i - 1); (j > -1); j--) {
-          matriz[j][comprimentoMatriz] -= matriz[j][i] * resultado[i];
-      }
+    resultado[i] = matriz[i][comprimentoMatriz] / matriz[i][i];
+    for (let j = (i - 1); (j > -1); j--) {
+      matriz[j][comprimentoMatriz] -= matriz[j][i] * resultado[i];
+    }
   }
 
   return resultado;
@@ -155,5 +158,4 @@ let teste = [
   [1, 2, 3, 14]
 ];
 let resultado = calcularGauss(teste, 0, 0);
-console.log(resultado);
-console.log(calcularX(resultado));
+console.log(JSON.stringify(resultado));
